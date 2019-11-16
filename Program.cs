45dbd4970bb1f978eae1b98d859da5a8acfaf314
adrian <adrian.rom64@gmail.com>
2019-11-16 16:24:50 +0100

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
